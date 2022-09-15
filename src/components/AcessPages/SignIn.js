f7a1dbe2cc79a542859d94becbfa6ac3a17f0401
Brunna Serafina <brunnaserafina@gmail.com>
2022-09-15 01:28:08 -0300

    feat: adding sign-in features

--- src/components/AcessPages/SignIn.js
+++ src/components/AcessPages/SignIn.js
@@ -1,12 +1,44 @@
 import { Container, Logo, Button } from "./AcessStyled";
 import Input from "../../common/Input";
 import { DiscIcon } from "../../common/Icons";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { postSignIn } from "../../services/backtothedisc";
+import MessageContext from "../../contexts/MessageContext";
 
 export default function SignIn() {
+  const navigate = useNavigate("");
+  const { setMessage } = useContext(MessageContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const body = {
+    email,
+    password,
+  };
+
+  function joinSignIn(event) {
+    event.preventDefault();
+
+    postSignIn(body)
+      .catch((err) => {
+        console.log(err);
+        setMessage({
+          type: "alert",
+          message: {
+            text: "Não foi possível efetuar o login! Tente novamente.",
+            type: "error",
+          },
+        });
+      })
+      .then((response) => {
+        localStorage.setItem(
+          "backtothedisc",
+          JSON.stringify({ token: response.data })
+        );
+
+        navigate("/my-cart");
+      });
+  }
 
   return (
     <Container>
@@ -15,7 +47,7 @@ export default function SignIn() {
         BACK TO THE <p></p> DISC
       </Logo>
 
-      <form>
+      <form onSubmit={joinSignIn}>
         <Input
           placeholder="E-mail"
           value={email}
